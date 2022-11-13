f30480d96bed38c0426162eeee28da89fdb6faff
zoicware <118035521+zoicware@users.noreply.github.com>
2022-11-12 21:29:55 -0500

    Add files via upload

---
+++ Delete temp.bat
@@ -0,0 +1,9 @@
+@echo off
+set folder="C:\Windows\Temp"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+
+set folder="%userprofile%\AppData\Local\Temp"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
